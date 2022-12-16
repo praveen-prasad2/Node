47234faec101024a3bde3f9a55fa20f2f416b8ec
Praveen-Prasad <praveenprasad2510@gmail.com>
2022-12-16 09:01:44 +0530

    Added status code to requests

--- 06-nodeapp.js
+++ 06-nodeapp.js
@@ -11,6 +11,7 @@ let fs=require("fs")
 let server=http.createServer((req,res)=> {
     if(req.url=='/'){
     // console.log("request from base url")
+    res.writeHead(200)
     res.write("<h1>Hello World</h1>")
     res.end()
 }
@@ -22,20 +23,29 @@ if(req.url=='/Praveen'){
 
 if(req.url=='/mypage'){
     fs.promises.readFile("index.html", 'utf-8').then((data) =>{
+        res.writeHead(200)
         res.write(data)
         res.end()
     })
     .catch((err) => {
+        res.writeHead(500)
           res.write("Page can't be reached")
           res.end()
       })}
 
 if(req.url=="/style.css"){
     fs.promises.readFile("style.css", 'utf-8').then((data) =>{
+        res.writeHead(200)
         res.write(data)
         res.end()
 })
 } 
     
 })
-server.listen(4000);
+server.listen(4000,(err)=>{
+    if(err){
+        console.log("Poyi matte terminal off cheyy")
+    }else{
+        console.log("Success")
+    }
+});
